5e2070fe0b50e6c135beddc09385b3e90fcf0dc4
claretnnamocha <devclareo@gmail.com>
2022-02-12 17:19:10 +0100

    remove swagger docs

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -2,4 +2,3 @@ export * as bullBoard from "./bull-board";
 export * as db from "./db";
 export * as env from "./env";
 export * as security from "./security";
-export * as swagger from "./swagger";
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,9 @@ import cors from "cors";
 import { config } from "dotenv";
 import express, { NextFunction, Request, Response, Router } from "express";
 import formdata from "express-form-data";
-import { db } from "./configs";
+import { db, security } from "./configs";
 import { response } from "./helpers";
 import routes from "./routes";
-import swaggerUi from "swagger-ui-express";
-import { security, swagger } from "./configs";
 
 export default ({ dbURL }) => {
   config();
@@ -17,8 +15,6 @@ export default ({ dbURL }) => {
   router.use(express.json({ limit: "100mb", type: "application/json" }));
   router.use(express.urlencoded({ limit: "100mb", extended: true }));
   router.use(cors());
-
-  routes.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swagger.config));
 
   security.lock(router);
 
